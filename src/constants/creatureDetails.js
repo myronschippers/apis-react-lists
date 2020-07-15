9a50f38f09e912ec730f05bff7fb89f849897ca9
Myron <dubtwee@gmail.com>
2020-07-15 12:56:14 -0500

    initial commit

---
+++ src/constants/creatureDetails.js
@@ -0,0 +1,24 @@
+const creatureDetails = [
+  {
+    name: 'Unicorn',
+    origin: 'England',
+  },
+  {
+    name: 'Sphinx',
+    origin: 'Egypt',
+  },
+  {
+    name: 'Kappa',
+    origin: 'Japan',
+  },
+  {
+    name: 'Dryad',
+    origin: 'Ireland',
+  },
+  {
+    name: 'Jackalope',
+    origin: 'Americas',
+  },
+];
+
+export default creatureDetails;
---
+++ src/constants/creatures.js
@@ -0,0 +1,9 @@
+const creatures = [
+  'Unicorn',
+  'Sphinx',
+  'Kappa',
+  'Dryad',
+  'Jackalope',
+];
+
+export default creatures;
